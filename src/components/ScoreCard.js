cc6e1904bbc6be3ba42a0676be2d6bcc33f935cd
cdsleaf <cdsleaf@gmail.com>
2019-02-01 16:49:37 +1300

    refactor: refactor ScoreCard component into stateless functional component

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { GiTrophyCup } from 'react-icons/gi';
 
-class ScoreCard extends Component {
-  render() {
-    const { user, medal } = this.props;
-    const { 
-      name, 
-      avatarURL,
-      scoreFromAnswered,
-      scoreFromCreated, 
-      totalScore 
-    } = user;
-    return (
-      <div className='score-card' >
-        <div className='card-avatar'>
-          <div className={'medal-icon ' + medal}>
-            {medal !== '' &&
-              <GiTrophyCup />
-            }
-          </div>   
-          <img src={process.env.PUBLIC_URL + avatarURL} alt="userAvatar" />
+const ScoreCard = () => {
+  const { user, medal } = this.props;
+  const { 
+    name, 
+    avatarURL,
+    scoreFromAnswered,
+    scoreFromCreated, 
+    totalScore 
+  } = user;
+
+  return (
+    <div className='score-card' >
+      <div className='card-avatar'>
+        <div className={'medal-icon ' + medal}>
+          {medal !== '' &&
+            <GiTrophyCup />
+          }
+        </div>   
+        <img src={process.env.PUBLIC_URL + avatarURL} alt="userAvatar" />
+      </div>
+      <div className='score-detail'>
+        <p>{name}</p> 
+        <p>Answered Questions <em>{scoreFromAnswered}</em></p>
+        <hr />
+        <p>Created Questions <em>{scoreFromCreated}</em></p>
+      </div>
+      <div className='total-score'>
+        <div className='total-score-title'>
+          <p>Score</p>
         </div>
-        <div className='score-detail'>
-          <p>{name}</p> 
-          <p>Answered Questions <em>{scoreFromAnswered}</em></p>
-          <hr />
-          <p>Created Questions <em>{scoreFromCreated}</em></p>
-        </div>
-        <div className='total-score'>
-          <div className='total-score-title'>
-            <p>Score</p>
-          </div>
-          <div className='total-score-point'>
-            <p>{totalScore}</p>
-          </div>
+        <div className='total-score-point'>
+          <p>{totalScore}</p>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default connect()(ScoreCard);
+export default ScoreCard;
