35fc8269e6266e16e7d47ef5233657b6feb8c59f
cdsleaf <cdsleaf@gmail.com>
2019-12-16 23:53:39 +1300

    refactor: adapt LeaderBoard to 'React Hook' & styled-component

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import ScoreCard from './ScoreCard';
+import styled from 'styled-components';
+import { device } from '../deviceBreakpoints';
 
-class LeaderBoard extends Component {
-  render() {
-    const { userList, medalColors } = this.props;
-    return (
-      <div className='leader-board'>
-        {userList.map((user, index) => (
-          <ScoreCard 
-            key={user.id} 
-            user={user} 
-            medal={medalColors[index] === undefined 
-              ? '' 
-              : medalColors[index]}/>
-        ))}
-      </div>
-    )
+const LeaderBoardMain = styled.div`
+  margin: 1rem auto;
+  padding: 1rem 1rem 0 1rem;
+
+  @media ${device.tablet} {
+    width: 50%;
+    margin: 1rem auto;
+    border: 0.1rem solid grey;
   }
+`;
+
+function LeaderBoard() {
+  const userList = useSelector(({ users }) => {
+    return Object.values(users)
+      .reduce((a, v) => {
+        const user = {
+          ...v,
+          scoreFromAnswered: Object.keys(v.answers).length,
+          scoreFromCreated: v.questions.length,
+          totalScore: Object.keys(v.answers).length + v.questions.length,
+        };
+        return [...a, user];
+      }, [])
+      .sort((a, b) => b.totalScore - a.totalScore);
+  });
+  const medalColors = [
+    'goldMedalColor',
+    'silverMedalColor',
+    'bronzeMedalColor',
+  ];
+
+  return (
+    <LeaderBoardMain>
+      {userList.map((user, index) => (
+        <ScoreCard
+          key={user.id}
+          user={user}
+          medal={medalColors[index] === undefined ? '' : medalColors[index]}
+        />
+      ))}
+    </LeaderBoardMain>
+  );
 }
 
-function mapStateToProps({ users }){
-  const userList = Object.values(users).reduce((a, v) => {
-    const user = {
-      ...v,
-      scoreFromAnswered: Object.keys(v.answers).length,
-      scoreFromCreated: v.questions.length,
-      totalScore:  Object.keys(v.answers).length + v.questions.length,
-    }
-    return [ ...a, user ]
-  }, []).sort((a,b) => b.totalScore-a.totalScore)
-  return {
-    userList,
-    medalColors: ['goldMedalColor', 'silverMedalColor', 'bronzeMedalColor'],
-  }
-}
-
-export default connect(mapStateToProps)(LeaderBoard);
+export default LeaderBoard;
